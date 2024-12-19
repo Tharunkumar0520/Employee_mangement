eb5456511d7f19ef93620eb70aacd27345e28925
Tharunkumar0520 <tharunknkumar@gmail.com>
2024-12-19 14:58:50 +0530

    added Docker

--- Server/index.js
+++ Server/index.js
@@ -1,14 +1,13 @@
 import express from "express";
 import { PrismaClient } from '@prisma/client'
-import cors from "cors";
 
 const client=new PrismaClient();
 const app=express();
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); // Allow any origin
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Allow specific HTTP methods
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allow specific headers
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
   });
   
